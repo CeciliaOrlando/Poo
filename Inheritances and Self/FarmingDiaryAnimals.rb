2f20d083bc0964d76615bd70bbe7b0d0d5263ad8
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-02-26 05:36:23 -0300

    animals ok

---
+++ Inheritances and Self/FarmingDiaryAnimals.rb
@@ -0,0 +1,60 @@
+#animal.rb
+class Animal
+  def initialize
+    @energy = 0
+  end
+
+  def feed!
+    @energy += 1
+  end
+end
+
+#chicken.rb
+require_relative "animal"
+
+class Chicken < Animal
+  attr_reader :eggs
+
+  def initialize(gender)
+    super()
+    @gender = gender
+    @eggs = 0
+  end
+
+  def feed!
+    super
+    @eggs += 2 if @gender == "female"
+  end
+
+  def talk
+    case @gender
+    when "male" then "cock-a-doodle-doo"
+    when "female" then "cluck cluck"
+    end
+  end
+end
+
+
+#cow.rb
+require_relative "animal"
+
+class Cow < Animal
+  attr_reader :milk
+
+  def initialize
+    super
+    @milk = 0
+  end
+
+  def feed!
+    super
+    @milk += 2
+  end
+
+  def talk
+    "moo"
+  end
+end
+
+
+
